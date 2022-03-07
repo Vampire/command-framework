6cc418f362e712a86ae680befea0cd9591b1a87a
Björn Kautler <Bjoern@Kautler.net>
2022-03-07 04:02:14 +0100

    Fix copyright years

--- buildSrc/src/main/kotlin/net/kautler/tests.gradle.kts
+++ buildSrc/src/main/kotlin/net/kautler/tests.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Bjoern Kautler
+ * Copyright 2019-2022 Bjoern Kautler
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
