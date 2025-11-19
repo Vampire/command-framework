a340e472b0e4d31f8baa265b44d5892c9d3a067e
Björn Kautler <Bjoern@Kautler.net>
2025-11-19 22:58:09 +0100

    Update Gradle daemon JVM to 21

--- gradle/build-logic/settings.gradle.kts
+++ gradle/build-logic/settings.gradle.kts
@@ -18,7 +18,7 @@ import org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_RE
 import org.gradle.api.initialization.resolve.RulesMode.FAIL_ON_PROJECT_RULES
 
 plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.10.0"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
 dependencyResolutionManagement {
