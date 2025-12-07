b1e5ad83d4db0e40d56b3c0f1ddc6aa2e7e55a88
Björn Kautler <Bjoern@Kautler.net>
2025-12-08 00:51:04 +0100

    Configure new publishing URLs

--- gradle/build-logic/src/main/kotlin/net/kautler/publishing.gradle.kts
+++ gradle/build-logic/src/main/kotlin/net/kautler/publishing.gradle.kts
@@ -156,6 +156,8 @@ signing {
 nexusPublishing {
     repositories {
         sonatype {
+            nexusUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/")
+            snapshotRepositoryUrl = uri("https://central.sonatype.com/repository/maven-snapshots/")
             stagingProfileId = sonatypeStagingProfileId
             username = sonatypeUsername
             password = sonatypePassword
